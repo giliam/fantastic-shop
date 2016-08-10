8f1027667bbc1fa4bf14ee02d5c3108760a37c9b
Matthieu Denoux <matthieu.denoux@gmail.com>
2016-08-10 23:54:16 +0200

    Works on permissions.

--- items/urls.py
+++ items/urls.py
@@ -1,12 +1,14 @@
-from django.conf.urls import url
+from django.conf.urls import url, include
 from rest_framework.urlpatterns import format_suffix_patterns
 from items import views
 
 urlpatterns = [
+    url(r'^$', views.api_root),
     url(r'^items/$', views.ItemList.as_view()),
     url(r'^items/(?P<pk>[0-9]+)/$', views.ItemDetail.as_view()),
     url(r'^users/$', views.UserList.as_view()),
     url(r'^users/(?P<pk>[0-9]+)/$', views.UserDetail.as_view()),
+    url(r'^api-auth/', include('rest_framework.urls', namespace='rest_framework')),
 ]
 
 urlpatterns = format_suffix_patterns(urlpatterns)
--- items/views.py
+++ items/views.py
@@ -1,11 +1,16 @@
 from items.models import Item
 from items.serializers import ItemSerializer, UserSerializer
-from rest_framework import generics
+from rest_framework import generics, permissions
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+from rest_framework.reverse import reverse
 from django.contrib.auth.models import User
+from items.permissions import IsOwnerOrReadOnly
 
 class ItemList(generics.ListCreateAPIView):
     queryset = Item.objects.all()
     serializer_class = ItemSerializer
+    permission_classes = (permissions.IsAuthenticatedOrReadOnly,IsOwnerOrReadOnly)
     
     def perform_create(self, serializer):
         serializer.save(owner=self.request.user)
@@ -13,6 +18,7 @@ class ItemList(generics.ListCreateAPIView):
 class ItemDetail(generics.RetrieveUpdateDestroyAPIView):
     queryset = Item.objects.all()
     serializer_class = ItemSerializer
+    permission_classes = (permissions.IsAuthenticatedOrReadOnly,IsOwnerOrReadOnly)
 
 
 class UserList(generics.ListAPIView):
@@ -23,3 +29,12 @@ class UserList(generics.ListAPIView):
 class UserDetail(generics.RetrieveAPIView):
     queryset = User.objects.all()
     serializer_class = UserSerializer
+
+
+
+@api_view(['GET'])
+def api_root(request, format=None):
+    return Response({
+        'users': reverse('users/', request=request, format=format),
+        'items': reverse('items/', request=request, format=format)
+    })
